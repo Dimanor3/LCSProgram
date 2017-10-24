17fc430ff6ee674739cc64bfce7c90ce4346c2ee
gbostonc <gbostonc@uncc.edu>
2017-10-24 19:18:37 -0400

    Just adding Comments

--- LCSMethodClass.java
+++ LCSMethodClass.java
@@ -1,7 +1,8 @@
+//hellloo there
 public class LCSMethodClass {
-	
+
 	/**
-	 * 
+	 *
 	 * @param str1
 	 * @param str2
 	 * @return lcs
@@ -11,7 +12,7 @@ public class LCSMethodClass {
 		int str1Length = str1.length();
 		int str2Length = str2.length();
 		int[][] table = new int[str1Length + 1][str2Length + 1];
-		
+
 		//bottom up, compare characters in X and Y
 		//fill the table with integers accordingly
 		for (int row = 0; row <= str1Length; row++)
@@ -26,11 +27,11 @@ public class LCSMethodClass {
 	                    table[row][column] = Math.max(table[row-1][column], table[row][column-1]);
 	            }
 	        }
-		
+
 		int find = table[str1Length][str2Length]; //current index, begin at far right corner of table
-		
+
 		char lcs[] = new char[find + 1]; //array of the characters in the lcs
-		
+
 		//move through the table, store chars in the lcs output array from the table
         int i = str1Length, j = str2Length;
         while (i > 0 && j > 0)
@@ -38,23 +39,23 @@ public class LCSMethodClass {
         	//if characters at indices are the same, corresponds to the diagonal movement in the table
             if (str1.charAt(i-1) == str2.charAt(j-1))
             {
-                lcs[find-1] = str1.charAt(i-1); 
+                lcs[find-1] = str1.charAt(i-1);
 
-                i--; 
-                j--; 
-                find--;     
+                i--;
+                j--;
+                find--;
             }
-  
+
             //if not the same, traverse left or up accordingly
             else if (table[i-1][j] > table[i][j-1])
                 i--;
-            
+
             else
                 j--;
         }
-        
+
         return lcs; //return the array of chars as the lcs
-        
+
 //        System.out.println("------------------------------------------------------------------");
 //        System.out.println("The LCS of DNA sequences: \n" + str1 + "\n" + str2 + "\n is: ");
 //        for (int k = 0; k < lcs.length - 1; k++)
